ed820dcfed2907e7f16138a07d6ce53bca579931
YogiK2001 <kaustubh7718@gmail.com>
2023-08-20 20:04:26 +0530

    Initial Commit

---
+++ controllers/signin.js
@@ -0,0 +1,30 @@
+const handleSignin = (db, bcrypt) => (req, res) => {
+    // if (req.body.email === database.users[0].email
+    //     && req.body.password === database.users[0].password) {
+    //     res.json(database.users[0]);
+    // } else {
+    //     res.status(400).json('error logging in');
+    // }
+    const { email, password } = req.body;
+    db.select('email', 'hash').from('login')
+        .where('email', '=', email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            }
+            else {
+                res.status(400).json('wrong credentials')
+            }
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
+}
+
+module.exports = {
+    handleSignin: handleSignin
+}
---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+const cors = require('cors');
+const knex = require('knex');
+const Clarifai = require('clarifai');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const PAT = '85896515269a47d7912e5e6b3fdab9c0';
+const USER_ID = '112yogi';
+const APP_ID = 'facereco';
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+
+const PORT = process.env.PORT;
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        port: 5432, // Updated to the correct PostgreSQL port
+        user: 'postgres',
+        password: 'guru',
+        database: 'smart-brain'
+    }
+});
+
+// db.select('*').from('users').then(data => {
+//     console.log(data);
+// });
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// const database = {
+//     users: [
+//         {
+//             id: '123',
+//             name: 'John',
+//             email: 'john@gmail.com',
+//             password: 'cookies',
+//             entries: 0,
+//             joined: new Date()
+//         },
+//         {
+//             id: '124',
+//             name: 'Sally',
+//             email: 'sally@example.com',
+//             password: 'banana',
+//             entries: 0,
+//             joined: new Date()
+//         },
+
+//     ]
+// }
+
+
+// app.get('/', (req, res) => {res.send(database.users);});
+app.post('/signin', (req, res) => { signin.handleSignin(db, bcrypt)(req, res) });
+app.post('/register', (req, res) => { register.registerHandler(req, res, db, bcrypt) });
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) });
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
+
+
+// // Load hash from your password DB.
+// bcrypt.compare(myPlaintextPassword, hash, function (err, result) {
+//     // result == true
+// });
+// bcrypt.compare(someOtherPlaintextPassword, hash, function (err, result) {
+//     // result == false
+// });
+
+/*F
+/ - create a fake db
+/ - add signin post request
+/ - add register post request
+/ - user middelware login
+/ - test with postman 
+
+/- Do it for the profile page
+/- Image PUT request
+*/
